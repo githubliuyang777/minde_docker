f40e1752decf73b9b74ec798a1fcdb2dc9ffb5c5
githubliuyang777 <yang_liu0418@163.com>
2025-01-07 11:30:06 +0800

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-source /usr/local/Ascend/mindie/latest/mindie-service/set_env.sh
+chmod +x /usr/local/Ascend/mindie/latest/mindie-service/set_env.sh
+chmod +x /usr/local/Ascend/mindie/latest/mindie-service/bin/mindieservice_daemon
+/usr/local/Ascend/mindie/latest/mindie-service/set_env.sh
 /usr/local/Ascend/mindie/latest/mindie-service/bin/mindieservice_daemon
